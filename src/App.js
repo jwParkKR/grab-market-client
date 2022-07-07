fbf4ec19ff3124c68a963e15861fc88a3c6505a2
jwParkKR <wjddnhw@naver.com>
2022-07-07 18:47:00 +0900

    Link 태그 적용

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import "./App.css";
 import MainPageComponent from "./main/index";
+import { Switch, Route } from "react-router-dom";
+import UploadPage from "./upload/index";
+import ProductPage from "./product/index";
 
 function App() {
-  return <MainPageComponent />;
+  return (
+    <div>
+      <Switch>
+        <Route exact={true} path={"/"}>
+          <MainPageComponent />
+        </Route>
+        <Route exact={true} path={"/products/:id"}>
+          <ProductPage />
+        </Route>
+        <Route exact={true} path={"/upload"}>
+          <UploadPage />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
